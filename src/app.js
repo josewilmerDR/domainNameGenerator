8a8f4f3132d7886331dfe7b8511e604732dee12e
josewilmerdr <josewdr@gmail.com>
2023-02-21 23:04:04 -0600

    proyect about domain name generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+var end = ['.com', '.es', '.cr']
+function domainNameGenerator(){
+  let theDomainName = "";
+  pronoun.forEach(i => {
+    adj.forEach(j => {
+      noun.forEach(k => {
+        end.forEach(l => {
+          theDomainName += i + j + k + l +"<br>"
+          document.getElementById("domain-name").innerHTML = '<p1>' + theDomainName + '</p1>'
+        })
+      })
+    })
+  })
+  return theDomainName;
+}
+
+domainNameGenerator();
